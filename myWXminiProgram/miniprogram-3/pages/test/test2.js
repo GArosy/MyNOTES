fb735f20f20d0fb482a78d53fd1649dc92a9f87b
Gsy <837225481@qq.com>
2022-01-23 02:02:00 +0800

    wxMiniProgram

--- myWXminiProgram/miniprogram-3/pages/test/test2.js
+++ myWXminiProgram/miniprogram-3/pages/test/test2.js
@@ -11,6 +11,12 @@ Page({
         console.log("clicked");
     },
 
+    navi(){
+        wx.navigateTo({
+          url: '/pages/test/test',
+        });
+    },
+
     /**
      * 生命周期函数--监听页面加载
      */
